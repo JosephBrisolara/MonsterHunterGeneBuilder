3783d329b30ae739485ed5d3b4177e922edc9fb5
Joseph Brisolara <josephbrisolara3035@gmail.com>
2024-03-27 09:31:45 -0500

    delete examples, start working on gene items

--- scripts/sortable.js
+++ scripts/sortable.js
@@ -1,21 +1,9 @@
 
 
 window.addEventListener("load", function () {
-
-    Sortable.create(firstList, {});
-
-    Sortable.create(simpleList, { /* options */ });
-
-
-    // List with handle
-    Sortable.create(listWithHandle, {
-        handle: '.glyphicon-move',
-        animation: 150
-    });
-
-
     Sortable.create(geneList, {
-        group: "genes"
+        group: "genes",
+        animation: 100
     });
     Sortable.create(geneGrid, {
         group: "genes"
