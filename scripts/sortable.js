2588dc072448d1baa0a14d4940471c3c93a387a2
Joseph Brisolara <josephbrisolara3035@gmail.com>
2024-04-06 09:50:38 -0500

    adding the put function for gene grid containers

--- scripts/sortable.js
+++ scripts/sortable.js
@@ -4,7 +4,17 @@ window.addEventListener("load", function () {
         group: {
             name: "genes"
         },
-        animation: 100
+        animation: 100,
+
+        // Restricting the ability to put multiple items from the gene list into the same gene grid container
+        // Element is dropped into the list from another list
+        onEnd: function (evt) {
+            destination = evt.to;
+            source = evt.from;
+
+            // Removing the padding from the gene grid container to better fit the gene card
+            destination.style.padding = 0;
+        }
     });
 
     // Add the ability to drag within the searched genes
@@ -18,10 +28,15 @@ window.addEventListener("load", function () {
 
     // Allow the genes to be put into the gene grid containers
     gridItemArray = document.getElementsByClassName('gridItem');
-    console.log(gridItemArray);
     for (var i = 0; i < gridItemArray.length; i++) {
         new Sortable(gridItemArray[i], {
-            group: 'genes',
+            group: {
+                name: 'genes',
+                put: function (to) {
+                    // restricting more than one gene per grid container
+                    return to.el.children.length < 1;
+                }
+            },
             animation: 150,
             fallbackOnBody: true,
         });
