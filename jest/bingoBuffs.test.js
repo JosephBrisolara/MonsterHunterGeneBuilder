4d28f40af6a088b768fc3ca88edb7b26be10c1f3
Joseph Brisolara <josephbrisolara3035@gmail.com>
2024-04-12 17:01:31 -0500

    start creating test for bingoBuffs

--- jest/bingoBuffs.test.js
+++ jest/bingoBuffs.test.js
@@ -1,6 +1,12 @@
 const bingoBuffs = require("../scripts/displayAllLoadouts");
 
 test('test all fire grid', () => {
+    const fireGenes = genes.filter((gene) => {
+        return gene.element == 'Fire';
+    });
 
-    expect(bingoBuffs(loadout)).toBe(biffDict);
+    // constructor(monsterName, topLeft, topMiddle, topRight, middleLeft, middleMiddle, middleRight, bottomLeft, bottomMiddle, bottomRight)
+    const loadout = Loadout('Fire Monster Name', fireGenes[0], fireGenes[1], fireGenes[2], fireGenes[3], fireGenes[4], fireGenes[5], fireGenes[6], fireGenes[7], fireGenes[8]);
+
+    expect(bingoBuffs(loadout)["Fire"]).toBe("8");
 });
