ce037ccc2143226f0b299cf9bbc4205dc9331961
Joseph Brisolara <josephbrisolara3035@gmail.com>
2024-04-12 17:08:09 -0500

    delete hamburger menu js, since it is no longer used

--- scripts/generalScripts.js
+++
@@ -1,9 +0,0 @@
-function hamburger() {
-    var menu = document.getElementById("navigation-links");
-    if (menu.style.display === "block") {
-        menu.style.display = "none";
-    }
-    else {
-        menu.style.display = "block";
-    }
-}
