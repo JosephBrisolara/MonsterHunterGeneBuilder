ebfda99278c958964a6c91830f6dc7839fb316c2
Joseph Brisolara <josephbrisolara3035@gmail.com>
2024-04-26 12:21:52 -0500

    formatting

--- scripts/home.js
+++ scripts/home.js
@@ -5,89 +5,90 @@ window.addEventListener("load", function () {
 
 });
 
+// need to break up into getting the gene array and displaying it
 function search() {
-        // Get all of the values from the input fields
-        const geneName = document.getElementById("searchGeneName");
-        const skill = document.getElementById("searchSkillName");
-        const fire = document.getElementById("fire");
-        const water = document.getElementById("water");
-        const ice = document.getElementById("ice");
-        const thunder = document.getElementById("thunder");
-        const dragon = document.getElementById("dragon");
-        const nonElem = document.getElementById("nonElem");
-        const power = document.getElementById("power");
-        const technical = document.getElementById("technical");
-        const speed = document.getElementById("speed");
-        const small = document.getElementById("small");
-        const medium = document.getElementById("medium");
-        const large = document.getElementById("large");
-        const xl = document.getElementById("XL");
-    
-        // Search for genes and output their genes to results
-        const results = genes.filter((gene) => {
-            const geneNameBool = (gene.name.includes(geneName.value.trim()) || (geneName.value.trim() == ''));
-            const skillNameBool = (gene.skillName.includes(skill.value.trim()) || (skill.value.trim() == ''));
+    // Get all of the values from the input fields
+    const geneName = document.getElementById("searchGeneName");
+    const skill = document.getElementById("searchSkillName");
+    const fire = document.getElementById("fire");
+    const water = document.getElementById("water");
+    const ice = document.getElementById("ice");
+    const thunder = document.getElementById("thunder");
+    const dragon = document.getElementById("dragon");
+    const nonElem = document.getElementById("nonElem");
+    const power = document.getElementById("power");
+    const technical = document.getElementById("technical");
+    const speed = document.getElementById("speed");
+    const small = document.getElementById("small");
+    const medium = document.getElementById("medium");
+    const large = document.getElementById("large");
+    const xl = document.getElementById("XL");
 
-            const geneElement = gene.element;
-            const fireBool = ((geneElement == fire.value) && (fire.checked));
-            const waterBool = ((geneElement == water.value) && (water.checked));
-            const iceBool = ((geneElement == ice.value) && (ice.checked));
-            const thunderBool = ((geneElement == thunder.value) && (thunder.checked));
-            const dragonBool = ((geneElement == dragon.value) && (dragon.checked));
-            const nonElemBool = ((geneElement == nonElem.value) && (nonElem.checked));
+    // Search for genes and output their genes to results
+    const results = genes.filter((gene) => {
+        const geneNameBool = (gene.name.includes(geneName.value.trim()) || (geneName.value.trim() == ''));
+        const skillNameBool = (gene.skillName.includes(skill.value.trim()) || (skill.value.trim() == ''));
 
-            const geneType = gene.type;
-            const powerBool = ((geneType == power.value) && (power.checked));
-            const technicalBool = ((geneType == technical.value) && (technical.checked));
-            const speedBool = ((geneType == speed.value) && (speed.checked));
+        const geneElement = gene.element;
+        const fireBool = ((geneElement == fire.value) && (fire.checked));
+        const waterBool = ((geneElement == water.value) && (water.checked));
+        const iceBool = ((geneElement == ice.value) && (ice.checked));
+        const thunderBool = ((geneElement == thunder.value) && (thunder.checked));
+        const dragonBool = ((geneElement == dragon.value) && (dragon.checked));
+        const nonElemBool = ((geneElement == nonElem.value) && (nonElem.checked));
 
-            const geneSize = gene.size;
-            const smallBool = ((geneSize == small.value) && (small.checked));
-            const mediumBool = ((geneSize == medium.value) && (medium.checked));
-            const largeBool = ((geneSize == large.value) && (large.checked));
-            const xlBool = ((geneSize == xl.value) && (xl.checked));
+        const geneType = gene.type;
+        const powerBool = ((geneType == power.value) && (power.checked));
+        const technicalBool = ((geneType == technical.value) && (technical.checked));
+        const speedBool = ((geneType == speed.value) && (speed.checked));
 
-            // No element checked on form
-            const elementBool = ((fire.checked == false) && (water.checked == false) && (ice.checked == false) && (thunder.checked == false) && (dragon.checked == false) && (nonElem.checked == false));
-            // No type checked on form
-            const typeBool = ((power.checked == false) && (technical.checked == false) && (speed.checked == false));
-            // No size checked on form
-            const sizeBool = ((small.checked == false) && (medium.checked == false) && (large.checked == false) && (xl.checked == false));
-            // No checkboxes filled on form
-            const checkboxBool = elementBool && typeBool && sizeBool;
+        const geneSize = gene.size;
+        const smallBool = ((geneSize == small.value) && (small.checked));
+        const mediumBool = ((geneSize == medium.value) && (medium.checked));
+        const largeBool = ((geneSize == large.value) && (large.checked));
+        const xlBool = ((geneSize == xl.value) && (xl.checked));
 
-            if (checkboxBool) {
-                return (geneNameBool && skillNameBool);
-            } else {
-                return geneNameBool && skillNameBool && (fireBool || waterBool || iceBool || thunderBool || dragonBool || nonElemBool || elementBool) && (powerBool || technicalBool || speedBool || typeBool) && (smallBool || mediumBool || largeBool || xlBool || sizeBool)
-            }
-        })
+        // No element checked on form
+        const elementBool = ((fire.checked == false) && (water.checked == false) && (ice.checked == false) && (thunder.checked == false) && (dragon.checked == false) && (nonElem.checked == false));
+        // No type checked on form
+        const typeBool = ((power.checked == false) && (technical.checked == false) && (speed.checked == false));
+        // No size checked on form
+        const sizeBool = ((small.checked == false) && (medium.checked == false) && (large.checked == false) && (xl.checked == false));
+        // No checkboxes filled on form
+        const checkboxBool = elementBool && typeBool && sizeBool;
 
-        // Generate Gene Cards to geneList
-        // Start by emptying geneList
-        const geneList = document.getElementById("geneList");
-        geneList.innerHTML = '';
-        // add the cards
-        for (let i = 0; i < results.length; i++) {
-            generateGeneCard(results[i], "geneList");
+        if (checkboxBool) {
+            return (geneNameBool && skillNameBool);
+        } else {
+            return geneNameBool && skillNameBool && (fireBool || waterBool || iceBool || thunderBool || dragonBool || nonElemBool || elementBool) && (powerBool || technicalBool || speedBool || typeBool) && (smallBool || mediumBool || largeBool || xlBool || sizeBool)
         }
+    })
 
-        // Clear all of the fields
-        geneName.value = " ";
-        skill.value = " ";
-        fire.checked = false;
-        water.checked = false;
-        ice.checked = false;
-        thunder.checked = false;
-        dragon.checked = false;
-        nonElem.checked = false;
-        power.checked = false;
-        technical.checked = false;
-        speed.checked = false;
-        small.checked = false;
-        medium.checked = false;
-        large.checked = false;
-        xl.checked = false;
+    // Generate Gene Cards to geneList
+    // Start by emptying geneList
+    const geneList = document.getElementById("geneList");
+    geneList.innerHTML = '';
+    // add the cards
+    for (let i = 0; i < results.length; i++) {
+        generateGeneCard(results[i], "geneList");
+    }
+
+    // Clear all of the fields
+    geneName.value = " ";
+    skill.value = " ";
+    fire.checked = false;
+    water.checked = false;
+    ice.checked = false;
+    thunder.checked = false;
+    dragon.checked = false;
+    nonElem.checked = false;
+    power.checked = false;
+    technical.checked = false;
+    speed.checked = false;
+    small.checked = false;
+    medium.checked = false;
+    large.checked = false;
+    xl.checked = false;
 }
 
 function saveLoadout() {
